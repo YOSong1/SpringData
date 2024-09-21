ed3fc9106b5858244e5c8355458b6158fb24c182
YOSong1 <43607595+YOSong1@users.noreply.github.com>
2024-09-21 14:00:35 +0900

    Delete SpringFramework/Chap2 directory

--- SpringFramework/Chap2/ch02_pjt_03/MyCalculator.java
+++
@@ -1,9 +0,0 @@
-package ch02_pjt_01;
-public class MyCalculator {
-
-    
-public void calculate(int fNum, int sNum, ICalculator calculator) {
-        int value = calculator.doOperation(fNum, sNum);       // 연산 실행
-        System.out.println("result : " + value);
-    }
-}
